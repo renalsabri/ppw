7a20464d9d8cd03fd8c445acf715411fef428fa5
patihh <fnfatih31@gmail.com>
2024-12-24 17:37:39 +0700

    webUpdate

--- beranda/beranda.php
+++ beranda/beranda.php
@@ -120,7 +120,7 @@ session_start();
                     ['name' => 'Action Figure Tobi', 'price' => 550000, 'image' => '../foto/Action Figure Tobi.jpg', 'link' => '../deskripsi/actionFigureTobi.php'],
                     ['name' => 'Jaket Kiana', 'price' => 300000, 'image' => '../foto/Jaket Kiana 1.jpg', 'link' => '../deskripsi/jaketKiana.php'],
                     ['name' => 'Replika Pedang Kirito', 'price' => 235000, 'image' => '../foto/Replika Pedang Kirito.jpg', 'link' => '../deskripsi/replikaPedangKirito.php'],
-                    ['name' => 'Hoodie Anime', 'price' => 332000, 'image' => '../foto/Hoodie Anime.jpg', 'link' => '../deskripsi/maskerWajah.php'],
+                    ['name' => 'Hoodie One Piece', 'price' => 90000, 'image' => '../foto/Hoodie One Piece.jpg', 'link' => '../deskripsi/hoodieOnePiece.php'],
                     ['name' => 'Gelang Elysia', 'price' => 109000, 'image' => '../foto/Gelang Elysia.jpg', 'link' => '../deskripsi/maskerWajah.php']
                 ];
 
